5a46abd5dbaa0c0a69e2422eac5546b5ff039a24
nanigasi_san <nanigasi.py@gmail.com>
2020-08-26 13:09:55 +0900

    api.mdをかくこと！

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify
 import db
 
 app = Flask(__name__)
@@ -18,15 +18,44 @@ def add_user():
     return jsonify({"message": f"add user {username}."}), 200
 
 
-@app.route("/user/login", methods = ["POST"])
+@app.route("/user/login", methods=["POST"])
 def login_user():
-    username, password_hash_md5=request.json["username"], request.json["password_hash"]
+    username, password_hash_md5 = request.json["username"], request.json["password_hash"]
     return jsonify({"check": db.check_password_hash(username, password_hash_md5)})
 
 
 @app.route("/user/list")
 def list_users():
     return jsonify(db.list_user_usernames())
+
+
+@app.route("/contest/add", methods=["POST"])
+def add_contest():
+    contest_name = request.json["contest_name"]
+    start = request.json["start"]
+    finish = request.json["finish"]
+    data_path = request.json["data_path"]
+    db.add_contest(contest_name, start, finish, data_path)
+    return jsonify({})
+
+
+@app.route("/contest/join", methods=["POST"])
+def join_contest():
+    contest_name = request.json["contest_name"]
+    username = request.json["username"]
+    db.join_contest(contest_name, username)
+    return jsonify({})
+
+
+@app.route("/contest/<contest_name>")
+def get_contest(contest_name):
+    data = db.get_contest_info(contest_name)
+    return jsonify(data)
+
+
+@app.route("/contest/standings/<contest_name>")
+def get_standings(contest_name):
+    return jsonify(db.get_standings(contest_name))
 
 
 app.run(debug=True)
--- db.py
+++ db.py
@@ -1,8 +1,8 @@
 import sqlite3
-from models import Contest
-
+from datetime import datetime
 
 datetime_format = "%Y/%m/%d-%H:%M:%S"
+
 
 class AutoCloseCursur(sqlite3.Cursor):
     def __init__(self, connection):
@@ -52,10 +52,10 @@ def list_user_usernames():
             return tuple((data[0] for data in cur.fetchall()))
 
 
-def add_contest(contest: Contest):
+def add_contest(contest_name: str, start: str, finish: str, data_path: str):
     with sqlite3.connect("db.sqlite3") as conn:
         with AutoCloseCursur(conn) as cur:
-            cur.execute("INSERT INTO Contests VALUES (?, ?, ?, ?)", (contest.contest_name, contest.period.start.strftime(datetime_format), contest.period.finish.strftime(datetime_format), contest.stock_data.data_path))
+            cur.execute("INSERT INTO Contests VALUES (?, ?, ?, ?)", (contest_name, start, finish, data_path))
         conn.commit()
 
 
@@ -64,6 +64,7 @@ def list_contests():
         with AutoCloseCursur(conn) as cur:
             cur.execute("SELECT * FROM Contests")
             return cur.fetchall()
+
 
 def join_contest(contest_name: str, username: str):
     with sqlite3.connect("db.sqlite3") as conn:
@@ -74,9 +75,23 @@ def join_contest(contest_name: str, username: str):
             cur.execute("INSERT INTO Standings VALUES (?, ?, ?)", (contest_name, username, 0))
         conn.commit()
 
+def get_contest_info(contest_name: str):
+    with sqlite3.connect("db.sqlite3") as conn:
+        with AutoCloseCursur(conn) as cur:
+            cur.execute("SELECT * FROM Contests WHERE contest_name=?", (contest_name, ))
+            data = cur.fetchone()
+            return {
+                "contest_name": data[0],
+                "start": data[1],
+                "finish": data[2],
+            }
 
 def get_standings(contest_name):
     with sqlite3.connect("db.sqlite3") as conn:
         with AutoCloseCursur(conn) as cur:
-            cur.execute("SELECT * FROM Standings WHERE contest_name=?", (contest_name, ))
-            return cur.fetchall()
+            cur.execute("SELECT username, score FROM Standings WHERE contest_name=?", (contest_name, ))
+            data = cur.fetchall()
+    standings = []
+    for username, score in data:
+        standings.append({"username": username, "score": score})
+    return standings
